84cca96ec2914e095673ae0ee8f123235711b7bc
喵喵大人 <support@catlib.io>
2017-09-25 11:57:03 +0800

    core update

---
+++ src/CatLib.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CatLib.Core.Tests")]
+[assembly: AssemblyDescription("CatLib Core Unit Tests")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CatLib")]
+[assembly: AssemblyProduct("CatLib.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright © CatLib 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c9f4024-910c-4881-a04d-34a6c3a09019")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/CatLib.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CatLib.Core")]
+[assembly: AssemblyDescription("CatLib Core")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CatLib")]
+[assembly: AssemblyProduct("CatLib.Core")]
+[assembly: AssemblyCopyright("Copyright © CatLib 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4204658e-81fd-4106-a347-890cd369c8a4")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Assembly-CSharp-Editor"),
+           InternalsVisibleTo("Assembly-CSharp-Editor-firstpass"),
+           InternalsVisibleTo("CatLib.Core.Tests")]
