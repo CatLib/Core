c5b82877ca222c42ed06937d3b5ea0ac3efb5951
喵喵大人 <support@catlib.io>
2017-12-07 20:38:35 +0800

    method container update

--- src/CatLib.Core/Support/Container/MethodContainer.cs
+++ src/CatLib.Core/Support/Container/MethodContainer.cs
@@ -9,6 +9,10 @@
  * Document: http://catlib.io/
  */
 
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
 namespace CatLib
 {
     /// <summary>
@@ -16,6 +20,16 @@ namespace CatLib
     /// </summary>
     internal sealed class MethodContainer
     {
+        /// <summary>
+        /// 调用方法目标 映射到 方法名字
+        /// </summary>
+        private Dictionary<object, List<string>> targetToMethodMappings;
+
+        /// <summary>
+        /// 方法映射
+        /// </summary>
+        private Dictionary<string, MethodInfo> methodMappings;
+
         /// <summary>
         /// 依赖注入容器
         /// </summary>
@@ -28,6 +42,8 @@ namespace CatLib
         internal MethodContainer(Container container)
         {
             this.container = container;
+            targetToMethodMappings = new Dictionary<object, List<string>>();
+            methodMappings = new Dictionary<string, MethodInfo>();
         }
 
         /// <summary>
@@ -39,7 +55,21 @@ namespace CatLib
         /// <returns></returns>
         public IMethodBindData BindMethod(string method, object target, string call)
         {
+            Guard.NotEmptyOrNull(method, "method");
+            Guard.Requires<ArgumentNullException>(target != null);
+            Guard.NotEmptyOrNull(call, "call");
             return null;
-        }        
+        }
+
+        /// <summary>
+        /// 生成对象中被调用方法的方法信息
+        /// </summary>
+        /// <param name="target">方法调用目标</param>
+        /// <param name="call">在方法调用目标中被调用的方法</param>
+        private MethodInfo MakeMethodInfo(object target, string call)
+        {
+            return target.GetType().GetMethod(call,
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.FlattenHierarchy);
+        }
     }
 }
