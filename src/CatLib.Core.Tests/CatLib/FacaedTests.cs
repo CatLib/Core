a5520aedd7328cc305e65cc9335511343fbecdd8
喵喵大人 <support@catlib.io>
2018-01-09 17:58:57 +0800

    facade support watch sync

--- src/CatLib.Core.Tests/CatLib/FacaedTests.cs
+++ src/CatLib.Core.Tests/CatLib/FacaedTests.cs
@@ -17,15 +17,90 @@ namespace CatLib.Tests
     [TestClass]
     public class FacaedTests
     {
+        public class FacaedTestClass : IFacaedTestClass
+        {
+        }
 
-        public class FacaedTestClass
+        public interface IFacaedTestClass
+        {
+        }
+        public class TestClassFacade : Facade<IFacaedTestClass>
         {
 
         }
 
-        public class TestClassFacaed : Facade<FacaedTestClass>
+        public class TestClassFacadeError : Facade<FacaedTestClass>
         {
+            
+        }
 
+        [TestMethod]
+        public void FacadeErrorTest()
+        {
+            var app = new Application();
+            app.Bootstrap();
+            app.Singleton<FacaedTestClass>();
+
+            var isError = false;
+            try
+            {
+                var data = TestClassFacadeError.Instance;
+            }
+            catch (TypeInitializationException)
+            {
+                isError = true;
+            }
+
+            Assert.AreEqual(true, isError);
+        }
+
+        [TestMethod]
+        public void FacadeWatchTest()
+        {
+            var app = new Application();
+            app.Bootstrap();
+            app.Singleton<FacaedTestClass>().Alias<IFacaedTestClass>();
+            var old = TestClassFacade.Instance;
+            app.Unbind<FacaedTestClass>();
+            app.Singleton<FacaedTestClass>().Alias<IFacaedTestClass>();
+
+            Assert.AreNotSame(old, TestClassFacade.Instance);
+        }
+
+        [TestMethod]
+        public void FacadeWatchTestWithInstance()
+        {
+            var app = new Application();
+            app.Bootstrap();
+            app.Singleton<FacaedTestClass>().Alias<IFacaedTestClass>();
+
+            var cls = new FacaedTestClass();
+            app.Instance<FacaedTestClass>(cls);
+
+            Assert.AreSame(cls, TestClassFacade.Instance);
+        }
+
+        [TestMethod]
+        public void FacadeMakeFaild()
+        {
+            var app = new Application();
+            app.Bootstrap();
+            app.Singleton<FacaedTestClass>().Alias<IFacaedTestClass>();
+            var old = TestClassFacade.Instance;
+
+            Assert.AreNotEqual(null, old);
+            app.Unbind<FacaedTestClass>();
+
+            var isError = false;
+            try
+            {
+                var data = TestClassFacade.Instance;
+            }
+            catch (UnresolvableException)
+            {
+                isError = true;
+            }
+            Assert.AreEqual(true, isError);
         }
 
         /// <summary>
@@ -36,15 +111,29 @@ namespace CatLib.Tests
         {
             var app = new Application();
             app.Bootstrap();
-            var obj = new FacaedTestClass();
+            IFacaedTestClass obj = new FacaedTestClass();
             app.Singleton<FacaedTestClass>((c, p) =>
             {
                 return obj;
-            });
+            }).Alias<IFacaedTestClass>();
 
-            Assert.AreEqual(obj, TestClassFacaed.Instance);
+            Assert.AreSame(obj, TestClassFacade.Instance);
             //double run
-            Assert.AreEqual(obj, TestClassFacaed.Instance);
+            Assert.AreSame(obj, TestClassFacade.Instance);
+            Assert.AreSame(obj, TestClassFacade.Instance);
+        }
+
+        [TestMethod]
+        public void FacadeReleaseTest()
+        {
+            var app = new Application();
+            app.Bootstrap();
+            app.Singleton<FacaedTestClass>().Alias<IFacaedTestClass>();
+
+            var data = TestClassFacade.Instance;
+            Assert.AreSame(data, TestClassFacade.Instance);
+            app.Release<IFacaedTestClass>();
+            Assert.AreNotSame(data, TestClassFacade.Instance);
         }
     }
 }
--- src/CatLib.Core/CatLib/Facade.cs
+++ src/CatLib.Core/CatLib/Facade.cs
@@ -14,16 +14,79 @@ namespace CatLib
     /// <summary>
     /// 门面
     /// </summary>
-    public abstract class Facade<T>
+    public abstract class Facade<TService>
     {
+        /// <summary>
+        /// 实例
+        /// </summary>
+        private static TService service;
+
+        /// <summary>
+        /// 绑定数据
+        /// </summary>
+        private static IBindable binder;
+
+        /// <summary>
+        /// 是否已经被初始化
+        /// </summary>
+        private static bool inited;
+
+        /// <summary>
+        /// 门面静态构造
+        /// </summary>
+        static Facade()
+        {
+            if (!typeof(TService).IsInterface)
+            {
+                throw new RuntimeException("Facade<" + typeof(TService).Name + "> , generic type must be interface.");
+            }
+
+            App.OnNewApplication += app =>
+            {
+                service = default(TService);
+                binder = null;
+                inited = false;
+            };
+        }
+
         /// <summary>
         /// 门面实例
         /// </summary>
-        public static T Instance
+        public static TService Instance
         {
             get
             {
-                return App.Make<T>();
+                if (service != null)
+                {
+                    return service;
+                }
+
+                Init();
+                return service = App.Make<TService>();
+            }
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        private static void Init()
+        {
+            if (!inited)
+            {
+                App.Watch<TService>((service) => Facade<TService>.service = service);
+                inited = true;
+            }
+
+            var newBinder = App.GetBind<TService>();
+            if (newBinder == null)
+            {
+                return;
+            }
+
+            if (binder == null || binder != newBinder)
+            {
+                newBinder.OnRelease((_, __) => service = default(TService));
+                binder = newBinder;
             }
         }
     }
