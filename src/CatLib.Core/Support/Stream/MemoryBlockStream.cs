a034f290666fd6c79128fd9651fbfdccfb2380c6
喵喵大人 <support@catlib.io>
2018-09-18 15:28:53 +0800

    add stream extension

---
+++ src/CatLib.Core/Support/Stream/MemoryBlockStream.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using System.IO;
+/*
+namespace CatLib
+{
+    
+    /// <summary>
+    /// 内存块数据流
+    /// </summary>
+    public class MemoryBlockStream : Stream
+    {
+        /// <summary>
+        /// 单个内存块的大小
+        /// </summary>
+        public int BlockSize { get; private set; }
+
+        /// <summary>
+        /// 内存块数据流
+        /// </summary>
+        /// <param name="blockSize">单个内存块的分块</param>
+        public MemoryBlockStream(int blockSize = 4096)
+        {
+            BlockSize = blockSize;
+        }
+
+        /// <summary>
+        /// 计算规定值最近的二的次幂的容量
+        /// </summary>
+        /// <param name="min">规定值值</param>
+        /// <returns>容量</returns>
+        private static int GetPrime(int min)
+        {
+            min = Math.Max(0, min);
+
+            var result = 8192;
+            for (var i = 2; i < int.MaxValue; i = i << 1)
+            {
+                if (i >= min)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
+*/
---
+++ src/CatLib.Core/Support/Util/StreamExtension.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using System.IO;
+
+namespace CatLib
+{
+    /// <summary>
+    /// Stream扩展函数
+    /// </summary>
+    public static class StreamExtension
+    {
+        /// <summary>
+        /// 默认的缓冲区
+        /// </summary>
+        [ThreadStatic]
+        private static readonly byte[] buffer;
+
+        /// <summary>
+        /// Stream扩展函数
+        /// </summary>
+        static StreamExtension()
+        {
+            buffer = new byte[4096];
+        }
+
+        /// <summary>
+        /// 将当前流追加到目标流中
+        /// </summary>
+        /// <param name="source">源数据流</param>
+        /// <param name="destination">目标数据流</param>
+        /// <returns>总共传输了多少数据</returns>
+        public static long AppendTo(this Stream source, Stream destination)
+        {
+            Guard.Requires<NullReferenceException>(source != null);
+            Guard.Requires<NullReferenceException>(destination != null);
+
+            long result = 0;
+            int read;
+            while ((read = source.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                destination.Write(buffer, 0, read);
+                result += read;
+            }
+
+            return result;
+        }
+    }
+}
