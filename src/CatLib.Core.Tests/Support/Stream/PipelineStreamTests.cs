4dd813b71ef8940cff572575506f546ef38e634d
喵喵大人 <support@catlib.io>
2018-10-09 16:02:27 +0800

    add PipelineStream

---
+++ src/CatLib.Core.Tests/Support/Stream/PipelineStreamTests.cs
@@ -0,0 +1,152 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CatLib.Core.Tests.Support.Stream
+{
+    [TestClass]
+    public class PipelineStreamTests
+    {
+        private PipelineStream stream;
+
+        [TestMethod]
+        public void TestPipeline()
+        {
+            stream = new PipelineStream(256);
+            ThreadPool.QueueUserWorkItem(WriteThread);
+
+            var wrote = false;
+            stream.OnWrote += (_) =>
+            {
+                wrote = true;
+            };
+            var data = new byte[100];
+            int read;
+            var actual = new StringBuilder();
+            var rand = new Random();
+            while ((read = stream.Read(data, 0, data.Length)) != 0)
+            {
+                var str = Encoding.UTF8.GetString(data, 0, read);
+                actual.Append(str);
+                Thread.Sleep(rand.Next(1, 5));
+            }
+            stream.Dispose();
+
+            var expected = new StringBuilder();
+            for (var i = 0; i < 1000; i++)
+            {
+                expected.Append("0123456789");
+            }
+           
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+            Assert.AreEqual(true, wrote);
+        }
+
+        public void WriteThread(object obj)
+        {
+            var data = Encoding.UTF8.GetBytes("0123456789");
+            for (var i = 0; i < 1000; i++)
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            stream.Close();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void TestClosedAndWrite()
+        {
+            var stream = new PipelineStream(256);
+            Assert.AreEqual(true, stream.CanWrite);
+            stream.Write(Encoding.UTF8.GetBytes("0123456789"), 0, 10);
+            stream.Close();
+            Assert.AreEqual(false, stream.CanWrite);
+            Assert.AreEqual(true, stream.CanRead);
+            Assert.AreEqual(true, stream.Closed);
+            stream.Write(Encoding.UTF8.GetBytes("0123456789"), 0, 10);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ObjectDisposedException))]
+        public void TestDisposeAndWrite()
+        {
+            var stream = new PipelineStream(256);
+            Assert.AreEqual(true, stream.CanWrite);
+            Assert.AreEqual(false, stream.CanRead);
+            stream.Dispose();
+            Assert.AreEqual(false, stream.CanWrite);
+            Assert.AreEqual(false, stream.CanRead);
+            stream.Write(Encoding.UTF8.GetBytes("0123456789"), 0, 10);
+        }
+
+        [TestMethod]
+        public void TestCanReadCanWrite()
+        {
+            var stream = new PipelineStream(256);
+            stream.Write(Encoding.UTF8.GetBytes("0123456789"), 0, 10);
+            Assert.AreEqual(true, stream.CanWrite);
+            Assert.AreEqual(true, stream.CanRead);
+        }
+
+        [TestMethod]
+        public void TestCanSeek()
+        {
+            var stream = new PipelineStream(256);
+            Assert.AreEqual(false, stream.CanSeek);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestSeek()
+        {
+            var stream = new PipelineStream(256);
+            stream.Seek(0, SeekOrigin.Begin);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestSetLength()
+        {
+            var stream = new PipelineStream(256);
+            stream.SetLength(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestSetPosition()
+        {
+            var stream = new PipelineStream(256);
+            stream.Position = 10;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestGetPosition()
+        {
+            var stream = new PipelineStream(256);
+            var pos = stream.Position;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestGetLength()
+        {
+            var stream = new PipelineStream(256);
+            var length = stream.Length;
+        }
+    }
+}
---
+++ src/CatLib.Core/Support/Stream/PipelineStream.cs
@@ -0,0 +1,325 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+using System.IO;
+using System.Threading;
+
+namespace CatLib
+{
+    /// <summary>
+    /// 管道通讯流
+    /// <para>一读一写线程安全</para>
+    /// </summary>
+    public class PipelineStream : Stream
+    {
+        /// <summary>
+        /// 可以被读取的长度
+        /// </summary>
+        private volatile int count;
+
+        /// <summary>
+        /// 容量
+        /// </summary>
+        private readonly int capacity;
+
+        /// <summary>
+        /// 休眠时间
+        /// </summary>
+        private readonly int sleep;
+
+        /// <summary>
+        /// 环形缓冲区
+        /// </summary>
+        private readonly RingBuffer ringBuffer;
+
+        /// <summary>
+        /// 当写入完成后触发
+        /// </summary>
+        public event Action<Stream> OnWrote;
+
+        /// <summary>
+        /// 是否已经被释放了
+        /// </summary>
+        private volatile bool disabled;
+
+        /// <summary>
+        /// 是否已经关闭流了
+        /// </summary>
+        private volatile bool closed;
+
+        /// <summary>
+        /// 总流量
+        /// </summary>
+        public long TotalFlow { get; private set; }
+
+        /// <summary>
+        /// 是否可以被读取
+        /// </summary>
+        public override bool CanRead
+        {
+            get { return count > 0 && !disabled; }
+        }
+
+        /// <summary>
+        /// 是否可以被写入
+        /// </summary>
+        public override bool CanWrite
+        {
+            get { return count < capacity && !closed; }
+        }
+
+        /// <summary>
+        /// 偏移位置（不支持）
+        /// </summary>
+        public override long Position
+        {
+            get { throw new NotSupportedException(); }
+            set { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// 流的长度
+        /// </summary>
+        public override long Length
+        {
+            get { throw new NotSupportedException(); }
+        }
+
+        /// <summary>
+        /// 是否能够设定偏移量
+        /// </summary>
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        /// <summary>
+        /// 是否已经关闭了流
+        /// </summary>
+        public bool Closed
+        {
+            get { return closed; }
+        }
+
+        /// <summary>
+        /// 管道通讯流
+        /// </summary>
+        /// <param name="capacity">缓冲区容量</param>
+        /// <param name="sleep">线程休眠时间</param>
+        public PipelineStream(int capacity = 4096, int sleep = 1)
+        {
+            this.capacity = capacity.ToPrime();
+            this.sleep = Math.Max(0, sleep);
+            TotalFlow = 0;
+            ringBuffer = new RingBuffer(this.capacity, false);
+        }
+
+        /// <summary>
+        /// GC回收时
+        /// </summary>
+        ~PipelineStream()
+        {
+            Dispose(!disabled);
+        }
+
+        /// <summary>
+        /// 偏移位置（不支持）
+        /// </summary>
+        /// <param name="offset">偏移量</param>
+        /// <param name="origin">偏移方向</param>
+        /// <returns></returns>
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// 设定流的长度（不支持）
+        /// </summary>
+        /// <param name="value">长度</param>
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        /// <summary>
+        /// 刷新缓冲区
+        /// </summary>
+        public override void Flush()
+        {
+            // ignore
+        }
+
+        /// <summary>
+        /// 将流中的数据读取到指定缓冲区
+        /// </summary>
+        /// <param name="buffer">指定缓冲区</param>
+        /// <param name="offset">缓冲区起始偏移量</param>
+        /// <param name="count">读取的长度</param>
+        /// <returns>实际读取的长度</returns>
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            while (true)
+            {
+                while (this.count <= 0)
+                {
+                    AssertDisabled();
+                    if (closed)
+                    {
+                        return 0;
+                    }
+                    Thread.Sleep(sleep);
+                }
+
+                AssertDisabled();
+
+                lock (ringBuffer.SyncRoot)
+                {
+                    AssertDisabled();
+                    if (this.count <= 0)
+                    {
+                        if (closed)
+                        {
+                            return 0;
+                        }
+                        continue;
+                    }
+
+                    try
+                    {
+                        var read = ringBuffer.Read(buffer, offset, count);
+                        this.count -= read;
+                        TotalFlow += read;
+                        return read;
+                    }
+                    finally
+                    {
+                        Guard.Requires<AssertException>(this.count >= 0);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 将指定缓冲区数据写入流中
+        /// </summary>
+        /// <param name="buffer">指定缓冲区</param>
+        /// <param name="offset">缓冲区起始偏移量</param>
+        /// <param name="count">写入的长度</param>
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            while (true)
+            {
+                while ((capacity - this.count) < count)
+                {
+                    AssertDisabled();
+                    AssertClosed();
+
+                    Thread.Sleep(sleep);
+                }
+
+                AssertDisabled();
+                AssertClosed();
+
+                lock (ringBuffer.SyncRoot)
+                {
+                    AssertDisabled();
+                    AssertClosed();
+
+                    if ((capacity - this.count) < count)
+                    {
+                        continue;
+                    }
+
+                    Guard.Requires<AssertException>(ringBuffer.Write(buffer, offset, count) == count);
+                    this.count += count;
+
+                    if (OnWrote != null)
+                    {
+                        OnWrote(this);
+                    }
+
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 关闭流
+        /// </summary>
+        public override void Close()
+        {
+            if (closed)
+            {
+                return;
+            }
+
+            lock (ringBuffer.SyncRoot)
+            {
+                closed = true;
+            }
+        }
+
+        /// <summary>
+        /// 断言关闭
+        /// </summary>
+        protected void AssertClosed()
+        {
+            if (closed)
+            {
+                throw new ObjectDisposedException("PipelineStream", "Stream is Closed Cannot write");
+            }
+        }
+
+        /// <summary>
+        /// 断言释放
+        /// </summary>
+        protected void AssertDisabled()
+        {
+            if (disabled)
+            {
+                throw new ObjectDisposedException("PipelineStream", "Stream is dispose");
+            }
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        /// <param name="disposing">是否进行释放</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (!disposing || disabled)
+            {
+                return;
+            }
+
+            lock (ringBuffer.SyncRoot)
+            {
+                if (disabled)
+                {
+                    return;
+                }
+
+                try
+                {
+                    disabled = true;
+                    closed = true;
+                    ringBuffer.Dispose();
+                }
+                finally
+                {
+                    base.Dispose(true);
+                }
+            }
+        }
+    }
+}
