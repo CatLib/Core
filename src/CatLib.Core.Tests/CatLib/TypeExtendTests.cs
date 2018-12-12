ac8fe8685bfff7de2a5ee6c13d49bfdd64884630
喵喵大人 <support@catlib.io>
2018-12-12 17:59:30 +0800

    resolved #105

---
+++ src/CatLib.Core.Tests/CatLib/TypeExtendTests.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) CatLib <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CatLib.Core.Tests
+{
+    [TestClass]
+    public class TypeExtendTests
+    {
+        [TestMethod]
+        public void TestGetService()
+        {
+            Assert.AreEqual(App.Type2Service<TypeExtendTests>(), typeof(TypeExtendTests).ToService());
+        }
+    }
+}
---
+++ src/CatLib.Core/CatLib/Extend/TypeExtend.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) CatLib <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System;
+
+namespace CatLib
+{
+    /// <summary>
+    /// Type类型扩展函数
+    /// </summary>
+    public static class TypeExtend
+    {
+        /// <summary>
+        /// 将类型转为服务名
+        /// </summary>
+        /// <param name="type">类型</param>
+        /// <returns>服务名</returns>
+        public static string ToService(this Type type)
+        {
+            return App.Type2Service(type);
+        }
+    }
+}
