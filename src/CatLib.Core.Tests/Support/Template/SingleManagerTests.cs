793aee891b660a2647315e6802362f67fea2b63b
喵喵大人 <support@catlib.io>
2018-12-12 18:00:49 +0800

    resolved #108

--- src/CatLib.Core.Tests/Support/Template/SingleManagerTests.cs
+++ src/CatLib.Core.Tests/Support/Template/SingleManagerTests.cs
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  * 
- * Document: http://catlib.io/
+ * Document: https://catlib.io/
  */
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
--- src/CatLib.Core/CatLib/Events/IGlobalDispatcher.cs
+++ src/CatLib.Core/CatLib/Events/IGlobalDispatcher.cs
@@ -6,7 +6,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this sender code.
  *
- * Document: http://catlib.io/
+ * Document: https://catlib.io/
  */
 
 using System;
