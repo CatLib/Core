c29b3b5286567d7f20961b739f28690e633e5e81
喵喵大人 <support@catlib.io>
2019-01-03 18:50:54 +0800

    resolved #152

--- src/CatLib.Core/CatLib/App.cs
+++ src/CatLib.Core/CatLib/App.cs
@@ -600,10 +600,11 @@ namespace CatLib
         /// 获取一个回调，当执行回调可以生成指定的服务
         /// </summary>
         /// <param name="service">服务名或别名</param>
+        /// <param name="userParams">用户提供的参数</param>
         /// <returns>回调方案</returns>
-        public static Func<object> Factory(string service)
+        public static Func<object> Factory(string service, params object[] userParams)
         {
-            return Handler.Factory(service);
+            return Handler.Factory(service, userParams);
         }
 
         /// <summary>
@@ -1345,17 +1346,6 @@ namespace CatLib
         public static Func<TService> Factory<TService>(params object[] userParams)
         {
             return Handler.Factory<TService>(userParams);
-        }
-
-        /// <summary>
-        /// 获取一个回调，当执行回调可以生成指定的服务
-        /// </summary>
-        /// <param name="service">服务名</param>
-        /// <param name="userParams">用户提供的参数</param>
-        /// <returns>回调方案</returns>
-        public static Func<object> Factory(string service, params object[] userParams)
-        {
-            return Handler.Factory(service, userParams);
         }
 
         /// <summary>
