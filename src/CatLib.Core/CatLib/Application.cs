0255df8ad09b026544b09528859ad9ccddc00745
喵喵大人 <support@catlib.io>
2019-01-14 16:36:11 +0800

    resolvde #168

--- src/CatLib.Core/CatLib/Application.cs
+++ src/CatLib.Core/CatLib/Application.cs
@@ -24,7 +24,7 @@ namespace CatLib
         /// <summary>
         /// 版本号
         /// </summary>
-        private static readonly Version version = new Version("1.3.0");
+        private static readonly Version version = new Version("1.3.1");
 
         /// <summary>
         /// 框架启动流程
